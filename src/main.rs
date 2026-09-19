f2cbe20eb9150eef379fe65466c564af72bb9053
ArthurPronota <yhgvnhjk.986ght.jhgt543@gmail.com>
2026-09-19 17:47:08 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::thread ;
 
 fn main() {
     let m = Arc::new(Mutex::new(false)) ;
+
     let m2 = m.clone() ;
 
     let h = thread::spawn(move || {
@@ -16,4 +17,20 @@ fn main() {
     h.join().unwrap() ;
 
     println!("{}", *m.lock().unwrap()) ;    // Out: true
+
+    // Второй вариант:
+
+    let h = thread::spawn({ // <- добавлена кавычка {
+        let m2 = m.clone() ; // создание клона
+        move || {
+            let mut g = m2.lock().unwrap() ;
+            if *g {
+                *g = !*g ;
+            }
+        }
+    }) ;
+
+    h.join().unwrap() ;
+
+    println!("{}", *m.lock().unwrap()) ;    // Out: false
 }
